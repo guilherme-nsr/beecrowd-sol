cd6d87a38172db1af0149a6c4e67b0cd500df570
Guilherme Nonato de Sá Ribeiro <nsr.guilherme@gmail.com>
2018-04-27 20:17:22 -0300

    '1008.py' adicionada

---
+++ 1008.py
@@ -0,0 +1,8 @@
+NUMBER = int(input())
+horas_trabalhadas = int(input())
+hora = float(input()) # O valor que o trabalhador recebe por hora
+
+SALARY = horas_trabalhadas * hora
+
+print("NUMBER =", NUMBER)
+print("SALARY = U$ %.2f" % SALARY)
