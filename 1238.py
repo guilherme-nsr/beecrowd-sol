ae8c2d13dd909150914426ff5e30347d6949bba3
Guilherme Nonato de Sá Ribeiro <nsr.guilherme@gmail.com>
2018-06-22 19:54:43 -0300

    Resolução questão 1238

---
+++ 1238.py
@@ -0,0 +1,37 @@
+def combinador(string1, string2):
+    tail = ''
+
+    if len(string1) > len(string2):
+        excedente = len(string1)-len(string2)
+        tail = string1[len(string1)-excedente:]
+        string1 = string1[:len(string1)-excedente]
+
+    elif len(string2) > len(string1):
+        excedente = len(string2)-len(string1)
+        tail = string2[len(string2)-excedente:]
+        string2 = string2[:len(string2)-excedente]
+
+    resultado = ''
+
+    for i in range(len(string1)):
+        char_s1 = string1[i]
+        char_s2 = string2[i]
+
+        resultado += char_s1
+        resultado += char_s2
+
+    resultado += tail
+
+    return resultado
+
+
+def main():
+    n = int(input())
+
+    for i in range(n):
+        string1, string2 = input().split()
+        print(combinador(string1, string2))
+
+
+if __name__ == "__main__":
+    main()
