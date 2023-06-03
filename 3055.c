eee0ebc9a875b1fc4440dfddc7305fefa73e6d7f
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2023-06-02 21:14:58 -0300

    solve 3055

---
+++ 3055.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int a, m;
+  scanf("%d %d", &a, &m);
+  printf("%d\n", 2*m - a);
+  return 0;
+}
---
+++ 3055.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a, m;
+  cin >> a >> m;
+  cout << 2*m - a <<endl;
+  return 0;
+}
