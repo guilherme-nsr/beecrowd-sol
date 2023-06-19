067ac2c73bfe2fe6dc4c18a83968d4c0da9de01a
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2023-06-19 01:09:53 -0300

    solve 3157

---
+++ 3157.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+  cout << "Nao se refuta ciencia com opiniao" << endl;
+  return 0;
+}
---
+++ 3157.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main() {
+  fmt.Printf("Nao se refuta ciencia com opiniao\n")
+}
