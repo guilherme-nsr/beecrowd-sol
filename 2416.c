1d939500323b007a4f29d5eeec5d9b07abe83639
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2023-12-30 14:55:29 -0300

    solve 2416 py, c and c++

---
+++ 2416.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+  int c, n;
+
+  scanf("%d %d", &c, &n);
+
+  printf("%d\n", c % n);
+
+  return 0;
+}
---
+++ 2416.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+ 
+  int c, n;
+  
+  cin >> c >> n;
+
+  cout << c % n << endl;
+
+  return 0;
+}
