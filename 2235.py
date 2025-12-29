1a27bf77d57fc881b64c46ff8bf9ff99a1744d5e
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2025-12-29 19:40:16 -0300

    solve 2235

---
+++ 2235.py
@@ -0,0 +1,12 @@
+def main():
+    a, b, c = map(int, input().split())
+    
+    if a-b == 0 or a-c == 0 or b-c == 0:
+        print('S')
+    elif a+b - c == 0 or a+c - b == 0 or b+c - a == 0:
+        print('S')
+    else:
+        print('N')
+    
+if __name__ == '__main__':
+    main()
