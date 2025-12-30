70ca8d79138d0c53ffa86fc4b0a2107314e24d61
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2025-12-30 13:30:20 -0300

    solve 2753

---
+++ 2753.py
@@ -0,0 +1,12 @@
+def main():
+    n = 97
+    
+    print('%d e %c' % (n, n))
+    
+    for i in range(25):
+        n += 1
+        print('%d e %c' % (n, n))
+        
+    
+if __name__ == '__main__':
+    main()
