0c6ab1957f014631bf5792d2949543eb5d768d0f
Guilherme Ribeiro <desaribeiro.guilherme@gmail.com>
2025-12-29 23:37:49 -0300

    solve 2780

---
+++ 2780.py
@@ -0,0 +1,13 @@
+def main():
+
+    d = int(input())
+
+    if d <= 800:
+        print(1)
+    elif d <= 1400:
+        print(2)
+    elif d <= 2000:
+        print(3)
+
+if __name__ == '__main__':
+    main()
